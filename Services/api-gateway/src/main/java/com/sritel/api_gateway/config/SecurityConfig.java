40501234b0de6c06e40f1fc8c998b7ef109e9600
Ravishan Jayathilake <jayathilakeravishan@gmail.com>
2024-09-29 09:36:04 +0000

    cors kelaweema fix kala

--- Services/api-gateway/src/main/java/com/sritel/api_gateway/config/SecurityConfig.java
+++ Services/api-gateway/src/main/java/com/sritel/api_gateway/config/SecurityConfig.java
@@ -1,32 +1,29 @@
 package com.sritel.api_gateway.config;
 
-import org.springframework.boot.autoconfigure.integration.IntegrationProperties.RSocket.Server;
+import org.springframework.http.HttpMethod; // Import the HttpMethod class
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.config.Customizer;
- 
 
 @Configuration
 @EnableWebFluxSecurity
-
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+        http.csrf().disable()
             .authorizeExchange(exchanges -> exchanges
-                .pathMatchers("/eurka/**")
+                .pathMatchers("/eureka/**") 
+                .permitAll()
+                .pathMatchers(HttpMethod.OPTIONS) 
                 .permitAll()
                 .anyExchange()
                 .authenticated()
-                
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
-            
     }
 }
