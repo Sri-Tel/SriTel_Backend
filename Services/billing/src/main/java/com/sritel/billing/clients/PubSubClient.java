4adcc101bf4192580aa08a1ce56f1e2ea3329adc
Ravishan Jayathilake <jayathilakeravishan@gmail.com>
2024-09-29 13:54:31 +0000

    fix

--- Services/billing/src/main/java/com/sritel/billing/clients/PubSubClient.java
+++ Services/billing/src/main/java/com/sritel/billing/clients/PubSubClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "pub-sub")
 public interface PubSubClient {
 
-    @PostMapping("api/billing/publish")
+    @PostMapping("/api/billing/publish")
     void publish(@RequestBody PublishRequest publishRequest);
 
 }
